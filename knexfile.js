ec861c905894f481bbe6753cd9b6ff928e2f2402
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-12 13:41:03 -0600

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/thailand',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+  }
+
+};
