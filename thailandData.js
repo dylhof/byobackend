27a9909fe856f82a27b81a56d0fe40d4c077ec35
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-12 15:35:52 -0600

    Add Thailand city and attractions data

---
+++ thailandData.js
@@ -0,0 +1,151 @@
+const thailandData = [
+  {
+    cityName: 'Bangkok',
+    avgMayHigh: 94,
+    avgMayLow: 79,
+    attractions: [
+      {
+        name: 'Grand Palace',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/grand-palace/a/poi-sig/1271084/357640'
+      },
+      {
+        name: 'Chatuchak Weekend Market',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/chatuchak-weekend-market/a/poi-sig/407334/357640'
+      },
+      {
+        name: 'Wat Pho',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/wat-pho/a/poi-sig/1148214/357640'
+      },
+      {
+        name: 'Wat Traimit (Golden Buddha)',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/wat-traimit-golden-buddha/a/poi-sig/1243931/357640'
+      },
+      {
+        name: 'Lumphini Park',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/lumphini-park/a/poi-sig/1244014/357640'
+      },
+      {
+        name: 'Suan Pakkad Palace Museum',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/suan-pakkad-palace-museum/a/poi-sig/1244039/357640'
+      },
+      {
+        name: 'Jim Thompson House',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/jim-thompson-house/a/poi-sig/407290/357640'
+      },
+      {
+        name: 'Wat Arun',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/wat-arun/a/poi-sig/407510/357640'
+      },
+      {
+        name: 'Siam Society & Kamthieng House',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/bangkok/attractions/siam-society-kamthieng-house/a/poi-sig/407748/357640'
+      }
+    ]
+  },
+  {
+    cityName: 'Chiang Mai',
+    avgMayHigh: 94,
+    avgMayLow: 74,
+    attractions: [
+      {
+        name: 'Elephant Nature Park',
+        linkToDetails: 'http://www.elephantnaturepark.org/'
+      },
+      {
+        name: 'Chiang Mai Cooking School',
+        linkToDetails: 'https://www.chiang-mai-cooking-school.com/'
+      },
+      {
+        name: 'Wat Phra That Doi Suthep',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/wat-phra-that-doi-suthep/a/poi-sig/1243262/357655'
+      },
+      {
+        name: 'Talat Warorot',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/talat-warorot/a/poi-sig/403644/357655'
+      },
+      {
+        name: 'Doi Suthep-Pui National Park',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/doi-suthep-pui-national-park/a/poi-sig/1155939/357655'
+      },
+      {
+        name: 'Wat Chedi Luang',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/wat-chedi-luang/a/poi-sig/1243143/357655'
+      },
+      {
+        name: 'Wat Phra Singh',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/wat-phra-singh/a/poi-sig/403724/357655'
+      },
+      {
+        name: 'Lanna Folklife Museum',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/lanna-folklife-museum/a/poi-sig/1445861/357655'
+      },
+      {
+        name: 'Wiang Kum Kam',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/wiang-kum-kam/a/poi-sig/1155916/357655'
+      },
+      {
+        name: 'Wat Chiang Man',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/wat-chiang-man/a/poi-sig/403712/357655'
+      },
+      {
+        name: 'Maiiam Contemporary Art Museum',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/chiang-mai/attractions/maiiam-contemporary-art-museum/a/poi-sig/1609657/357655'
+      },
+    ]
+  },
+  {
+    cityName: 'Ko Samui',
+    avgMayHigh: 83,
+    avgMayLow: 73,
+    attractions: [
+      {
+        name: 'Na Muang Waterfalls',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/na-muang-waterfalls/a/poi-sig/1243057/357722'
+      },
+      {
+        name: 'View Top',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/view-top/a/poi-sig/1582586/357722'
+      },
+      {
+        name: 'Wat Plai Laem',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/wat-plai-laem/a/poi-sig/1582659/357722'
+      },
+      {
+        name: 'Fishermans Village',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/fishermans-village/a/poi-sig/1583383/357722'
+      },
+      {
+        name: 'Ban Hua Thanon',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/ban-hua-thanon/a/poi-sig/1243048/357722'
+      },
+      {
+        name: 'Wat Racha Thammaram',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/wat-racha-thammaram/a/poi-sig/1582269/357722'
+      },
+      {
+        name: 'Sea Kayaking at Ang Thong National Marine Par',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/activities/sea-kayaking-at-ang-thong-national-marine-park-from-koh-samui/a/pa-act/v-3685SMU25/357722'
+      },
+      {
+        name: 'Coconut Museum',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/coconut-museum/a/poi-sig/1582329/357722'
+      }, 
+      {
+        name: 'Nam Tok Hin Lat',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/nam-tok-hin-lat/a/poi-sig/1243058/357722'
+      },
+      {
+        name: 'Mae Nam Beach',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/mae-nam-beach/a/poi-sig/1582578/357722'
+      },
+      {
+        name: 'Tan Rua Waterfall',
+        linkToDetails: 'https://www.lonelyplanet.com/thailand/attractions/tan-rua-waterfall/a/poi-sig/1582577/357722'
+      }
+    ]
+  },
+];
+
+module.exports = {
+  thailandData
+};
